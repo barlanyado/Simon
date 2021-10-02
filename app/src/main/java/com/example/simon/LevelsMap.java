61a1a0526c7b62b3b4fa5a9b4abd44f7b1453715
lior <liorbendayan@gmail.com>
2021-10-02 16:24:45 +0300

    Commit and push

--- app/src/main/java/com/example/simon/LevelsMap.java
+++ app/src/main/java/com/example/simon/LevelsMap.java
@@ -1,9 +1,15 @@
 package com.example.simon;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.drawable.GradientDrawable;
@@ -12,107 +18,206 @@ import android.graphics.drawable.shapes.RectShape;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RatingBar;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+import java.util.HashMap;
 
 public class LevelsMap extends AppCompatActivity {
-    int level_number = 12;
+
+    final private int LEVELS_NUMBER = 12;
+    final private int STARS_NUMBER = 3;
+    final private int LEVELS_IN_ROW = 4;
+    final private int ROWS_NUMBER = 3;
+
+    private ActivityResultLauncher<Intent> resultLauncher;
+    private HashMap<Integer, RatingBar> ratings = new HashMap<>();
+    private HashMap<Integer, Button> buttons = new HashMap<>();
+    private HashMap<Integer, TableRow> rows = new HashMap<>();
+    private TableLayout tableLayout;
+    private SharedPreferences sp;
+    private int current_level = 1;
+    private boolean result_succeed = false;
+    private int result_seconds = 0;
 
     @SuppressLint({"SetTextI18n", "UseCompatLoadingForDrawables"})
     public int dpToPx(Context context, float dp) {
         return Math.round(dp * context.getResources().getDisplayMetrics().density);
     }
 
-    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_levels_map);
+        tableLayout = findViewById(R.id.root_levels_table_layout);
+        initLauncher();
 
-        LinearLayout levels_layout = findViewById(R.id.levels);
+        sp = getSharedPreferences("levels", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        //resetMap(editor);
+        editor.putBoolean(openedString(1), true);
+        editor.putInt(starsString(1), 0);
+        editor.commit();
 
-        LinearLayout.LayoutParams levelsRow_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        levelsRow_params.setMargins(dpToPx(this,5),dpToPx(this,10),dpToPx(this,5),0);
+        initMap();
 
-        LinearLayout.LayoutParams ratingRow_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        ratingRow_params.setMargins(dpToPx(this,5),dpToPx(this,10),0,0);
+    }
 
-        LinearLayout.LayoutParams buttons_params = new LinearLayout.LayoutParams(dpToPx(this,50), dpToPx(this,50));
-        buttons_params.setMargins(dpToPx(this,25),dpToPx(this,10),dpToPx(this,20),0);
-
-        LinearLayout levels_row = new LinearLayout(this);
-        levels_row.setLayoutParams(levelsRow_params);
-        levels_row.setOrientation(LinearLayout.HORIZONTAL);
-
-        levels_layout.addView(levels_row);
-
-        GradientDrawable gd = new GradientDrawable();
-        gd.setCornerRadius(10);
-        gd.setStroke(4, Color.WHITE);
-
-        Button btn = new Button(this);
-        btn.setText("1");
-        btn.setTextColor(Color.WHITE);
-        btn.setLayoutParams(buttons_params);
-        btn.setId(1);
-        btn.setBackground(gd);
-        levels_row.addView(btn);
-
-        LinearLayout rating_row = new LinearLayout(this);
-        rating_row.setLayoutParams(ratingRow_params);
-        rating_row.setOrientation(LinearLayout.HORIZONTAL);
-
-        levels_layout.addView(rating_row);
-
-        LinearLayout.LayoutParams rb_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
-        rb_params.setMargins(dpToPx(this,25),0,dpToPx(this,23),0);
-
-
-        RatingBar rb = new RatingBar(this,null,R.attr.ratingBarStyleSmall);
-        rb.setLayoutParams(rb_params);
-        rb.setNumStars(3);
-        rating_row.addView(rb);
-        rb.setId(101);
-        int i = 2;
-
-        for(; i <= level_number; i++)
-        {
-            if(i % 4 == 1)
-            {
-                levels_row = new LinearLayout(this);
-                levels_row.setLayoutParams(levelsRow_params);
-                levels_row.setOrientation(LinearLayout.HORIZONTAL);
-
-                rating_row = new LinearLayout(this);
-                rating_row.setLayoutParams(ratingRow_params);
-                rating_row.setOrientation(LinearLayout.HORIZONTAL);
-
-                levels_layout.addView(levels_row);
-                levels_layout.addView(rating_row);
+    private void initLauncher()
+    {
+        resultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK)
+                {
+                    Intent data = result.getData();
+                    result_succeed = data.getBooleanExtra("result_succeed", false);
+                    result_seconds = (int)data.getFloatExtra("result_seconds", 0);
+                }
             }
-            gd = new GradientDrawable();
-            gd.setCornerRadius(10);
-            gd.setColor(Color.GRAY);
 
-            btn = new Button(this);
-            btn.setText(String.valueOf(i));
-            btn.setLayoutParams(buttons_params);
-            btn.setId(i);
-            btn.setBackground(gd);
-            btn.setTextColor(Color.WHITE);
-            btn.setClickable(false);
-            levels_row.addView(btn);
+        });
+    }
 
-            rb = new RatingBar(this,null,R.attr.ratingBarStyleSmall);
-            rb.setLayoutParams(rb_params);
-            rb.setNumStars(3);
-            rb.setId((100 + i));
-            rating_row.addView(rb);
-
+    private void initMap()
+    {
+        // Init table rows
+        for (int r = 0; r < ROWS_NUMBER; r++)
+        {
+            TableRow newRow = new TableRow(this);
+            TableLayout.LayoutParams rowLayParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT );
+            rowLayParams.weight = 1.0f;
+            newRow.setLayoutParams(rowLayParams);
+            newRow.setId(View.generateViewId());
+            rows.put(r, newRow);
+            tableLayout.addView(newRow);
         }
 
-        RatingBar test = findViewById(101);
-        test.setRating(3);
+
+        int hashCounter = 0;
+        for (int r = 0; r < ROWS_NUMBER; r++)
+        {
+            TableRow row = rows.get(r);
+            for (int l = 0; l < LEVELS_IN_ROW; l++)
+            {
+                TableRow.LayoutParams levelsRow_params = new TableRow.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                levelsRow_params.setMargins(0,dpToPx(this,25),0,0);
+                levelsRow_params.gravity = Gravity.CENTER;
+                levelsRow_params.weight = LEVELS_IN_ROW / ROWS_NUMBER;
+
+                LinearLayout cellLayout = new LinearLayout(this);
+                cellLayout.setLayoutParams(levelsRow_params);
+                cellLayout.setOrientation(LinearLayout.VERTICAL);
+
+                GradientDrawable gd = new GradientDrawable();
+                gd.setCornerRadius(10);
+                gd.setColor(Color.GRAY);
+
+                TableRow.LayoutParams buttons_params = new TableRow.LayoutParams(dpToPx(this,50), dpToPx(this,50));
+                buttons_params.setMargins(0,0,0,0);
+                buttons_params.gravity = Gravity.CENTER;
+                Button btn = new Button(this);
+                btn.setText(String.valueOf(hashCounter + 1));
+                btn.setLayoutParams(buttons_params);
+                btn.setId(View.generateViewId());
+                btn.setBackground(gd);
+                btn.setTextColor(Color.WHITE);
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(LevelsMap.this, SimonLogic.class);
+                        intent.putExtra("level", Integer.valueOf(String.valueOf(((Button)v).getText())));
+                        resultLauncher.launch(intent);
+                    }
+                });
+                btn.setClickable(false);
+                buttons.put(hashCounter, btn);
+                cellLayout.addView(btn);
+
+                TableRow.LayoutParams rb_params = new TableRow.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+                rb_params.setMargins(0,15,0,0);
+                rb_params.gravity = Gravity.CENTER;
+                RatingBar rb = new RatingBar(this,null,R.attr.ratingBarStyleSmall);
+                rb.setLayoutParams(rb_params);
+                rb.setNumStars(STARS_NUMBER);
+                rb.setId(View.generateViewId());
+                ratings.put(hashCounter, rb);
+                cellLayout.addView(rb);
+
+                row.addView(cellLayout);
+                hashCounter++;
+            }
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (result_succeed)
+        {
+            sp = getSharedPreferences("levels", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putInt(starsString(current_level), calculateScore());
+            current_level++;
+            editor.putBoolean(openedString(current_level), true);
+            editor.commit();
+        }
+
+        sp = getSharedPreferences("levels", MODE_PRIVATE);
+        for (int l = 0; l < LEVELS_NUMBER; l++)
+        {
+            if (sp.getBoolean(openedString(l + 1), false))
+            {
+                Button btn = buttons.get(l);
+                btn.setClickable(true);
+
+                GradientDrawable gd = new GradientDrawable();
+                gd.setCornerRadius(10);
+                gd.setStroke(4, Color.WHITE);
+                btn.setBackground(gd);
+
+                RatingBar rating = ratings.get(l);
+                rating.setRating(sp.getInt(starsString(l + 1), 0));
+
+            }
+        }
+    }
+
+    private String openedString(int level)
+    {
+        return ("level_" + String.valueOf(level) + "_opened");
+    }
+
+    private String starsString(int level)
+    {
+        return ("level_" + String.valueOf(level) + "_stars");
+    }
+
+    private int calculateScore()
+    {
+        if (result_seconds < 2)
+            return  3;
+        else if (result_seconds < 7)
+            return 2;
+        else if (result_seconds < 10)
+            return 1;
+        else
+            return 0;
+    }
+
+    public void resetMap(SharedPreferences.Editor editor)
+    {
+        for (int i = 1; i <= LEVELS_NUMBER; i++)
+        {
+            editor.putBoolean(openedString(i), false);
+            editor.putInt(starsString(i), 0);
+        }
+        editor.commit();
+    }
+
 }
